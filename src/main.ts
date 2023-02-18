fa0d5cf8e64c040f57198a72c67b79f2986d8b70
rahulshamkuwar <rahulshamkuwar20@gmail.com>
2023-02-17 23:49:59 -0700

    Fixed some validation errors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      validatorPackage: require("@nestjs/class-validator"),
+      transformerPackage: require("@nestjs/class-transformer"),
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
